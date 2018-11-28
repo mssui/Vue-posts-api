652458e7d79be1f086af00d7a9eefe57d101d4b3
ASLI ARI <asliari2012@gmail.com>
2018-11-28 12:01:33 +0100

    Delete ekstra lines

--- server.js
+++ server.js
@@ -8,20 +8,19 @@ app.use(express.static(__dirname + '/public'));
 
 //register a handler. When someone visit spesific route we can send a HTML page or jason file
 
-app.get('/', (req, res)=> {
+app.get('/data', (req, res)=> {
     res.send({
         name : 'Asli',
         hobbies : ['cooking', 'herbal recepies', 'dancing'],
         location: 'Berlin'
     });
 
-});  // iki parametre alır. ilki URL, ikinci Fonskiyon. Fonksiyon da 2 parametre alır. Reuest ve Respond
+});  
 
 app.get('/about', (req, res)=>{
     res.send('Bu benim about sayfam olmalı');
 });
 
-//Get handlerımız yukarıdaki gibi. Ama çalışması için bir port belirtmemiz lazım. Bu local da olabilir online da. Aşağıdaki app.listen'ı mutlaka belirtiyoruz.
 
 app.listen(port, ()=>{
     console.log('Server is running on :', port)
